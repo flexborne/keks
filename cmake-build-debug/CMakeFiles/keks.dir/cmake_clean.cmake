8842645702e859ac4dfa1fbb7d0bf10227c438f8
aleksandr.shrubkovskyi <aleksandr.shrubkovskyi@gmail.com>
2022-02-16 00:09:47 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/keks.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/keks.dir/main.cpp.obj"
+  "keks.exe"
+  "keks.exe.manifest"
+  "keks.pdb"
+  "libkeks.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/keks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/keks.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for keks.
+# This may be replaced when dependencies are built.
